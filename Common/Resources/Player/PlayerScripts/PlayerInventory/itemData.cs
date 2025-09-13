c44c1f2cbe4ea2e5fcb8f5d7b6ca12321a7c5eef
Lucas Nogueira Policano <128404230+JustALittleBug@users.noreply.github.com>
2025-09-13 19:07:09 -0300

    Update itemData.cs

--- Common/Resources/Player/PlayerScripts/PlayerInventory/itemData.cs
+++ Common/Resources/Player/PlayerScripts/PlayerInventory/itemData.cs
@@ -1,12 +1,14 @@
 using Godot;
-using PlayerScript.PlayerInventory;
 
-[GlobalClass]
-public partial class ItemData : Resource
+namespace PlayerScript.PlayerInventory
 {
-    [Export] public string ItemID;
-    [Export] public string Name;
-    [Export] public Texture2D Icon;
-    [Export] public ItemType Type;
-    [Export] public int SlotPerItem = 1;
-}
+    [GlobalClass]
+    public partial class ItemData : Resource
+    {
+        [Export] public string ItemID;
+        [Export] public string Name;
+        [Export] public Texture2D Icon;
+        [Export] public ItemType Type;
+        [Export] public int SlotPerItem = 1;
+    }
+};
